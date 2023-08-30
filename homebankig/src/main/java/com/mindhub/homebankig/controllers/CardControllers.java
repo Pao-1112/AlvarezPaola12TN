2534d7ad41b6a9ef47819e770ae382c57c8a83a5
Pao-1112 <pao.genshin.11@gmail.com>
2023-08-30 16:08:21 -0300

    Version.4

--- homebankig/src/main/java/com/mindhub/homebankig/controllers/CardControllers.java
+++ homebankig/src/main/java/com/mindhub/homebankig/controllers/CardControllers.java
@@ -63,29 +63,61 @@ public class CardControllers {
         Client client = clientRepository.findByEmail(authentication.getName());
         Set<Card> cards = client.getCards();
         long devitCounter = cards.stream().filter(card -> card.getType() == CardType.DEBIT).count();
-        long creditCount = cards.stream().filter(card -> card.getType() == CardType.CREDIT).count();
+        long creditCounter = cards.stream().filter(card -> card.getType() == CardType.CREDIT).count();
 
-        if (devitCounter >= 3 || creditCount >= 3 ){
+        CardType cardType1 = CardType.DEBIT;
+        CardType cardType2 = CardType.CREDIT;
+
+        CardColor color1 = CardColor.GOLD;
+        CardColor color2 = CardColor.SILVER;
+        CardColor color3 = CardColor.TITANIUM;
+
+        String cardNumber = getCardNumbers();
+        int cvv = getRandomCvvNumber(100, 999);
+
+        LocalDateTime thruDate = LocalDateTime.now();
+        LocalDateTime fromDate = thruDate.plusYears(5);
+
+        if(devitCounter >= 3 || creditCounter >= 3 ){
             return new ResponseEntity<>("403 forbidden", HttpStatus.FORBIDDEN);
-        }else {
-            CardType cardType = CardType.DEBIT;
-            CardType cardType1 = CardType.CREDIT;
-
-            String cardNumber = getCardNumbers();
-            int cvv = getRandomCvvNumber(100, 999);
-
-            LocalDateTime thruDate = LocalDateTime.now();
-            LocalDateTime fromDate = thruDate.plusYears(5);
-
-            Card card;
-            Card card1;
-            card = new Card(client.getFirstName() + " " + client.getLastName(), cardType, color, cardNumber, cvv, thruDate, fromDate);
-            client.addCard(card);
-            cardRepository.save(card);
-            card1 = new Card(client.getFirstName() + " " + client.getLastName(), cardType1, color, cardNumber, cvv, thruDate, fromDate);
-            client.addCard(card1);
-            cardRepository.save(card1);
-            return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+        }else{
+            if (color == color1 && type == cardType1) {
+                Card card1;
+                card1 = new Card(client.getFirstName() + " " + client.getLastName(), cardType1, color1, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card1);
+                cardRepository.save(card1);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            } if(color == color1 && type == cardType2){
+                Card card2;
+                card2 = new Card(client.getFirstName() + " " + client.getLastName(), cardType2, color1, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card2);
+                cardRepository.save(card2);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            } if(color == color2 && type == cardType1) {
+                Card card3;
+                card3 = new Card(client.getFirstName() + " " + client.getLastName(), cardType1, color2, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card3);
+                cardRepository.save(card3);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            } if(color == color2 && type == cardType2) {
+                Card card4;
+                card4 = new Card(client.getFirstName() + " " + client.getLastName(), cardType2, color2, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card4);
+                cardRepository.save(card4);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            } if(color == color3 && type == cardType1){
+                Card card5;
+                card5 = new Card(client.getFirstName() + " " + client.getLastName(), cardType1, color3, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card5);
+                cardRepository.save(card5);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            }else {
+                Card card6;
+                card6 = new Card(client.getFirstName() + " " + client.getLastName(), cardType2, color3, cardNumber, cvv, thruDate, fromDate);
+                client.addCard(card6);
+                cardRepository.save(card6);
+                return new ResponseEntity<>("201 created", HttpStatus.CREATED);
+            }
         }
     }
 }
